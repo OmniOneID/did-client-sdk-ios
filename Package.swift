bc61c78692c4621638166be72309668abfc88455
JooHyun <jhpark0512@raoncorp.com>
2025-10-27 17:21:34 +0900

    feat: modiy Package file

--- Package.swift
+++ Package.swift
@@ -11,18 +11,28 @@ let package = Package(
         .library(name: "DIDWalletSDK", targets: ["DIDWalletSDK"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/apple/swift-collections.git",
-            from: "1.1.4"
-        )
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.4")
     ],
     targets: [
+        .binaryTarget(
+            name: "OpenSSL",
+            path: "source/did-wallet-sdk-ios/DIDWalletSDK/Framework/openssl.xcframework"
+        ),
+
         .target(
             name: "OpenSSLWrapper",
+            dependencies: ["OpenSSL"],
             path: "source/did-wallet-sdk-ios/DIDWalletSDK/Core/SubComponent/Bridging",
             publicHeadersPath: ".",
             cSettings: [
-                .headerSearchPath(".")
+                .headerSearchPath(
+                    "../../../Framework/openssl.xcframework/ios-arm64/openssl.framework/Headers",
+                    .when(platforms: [.iOS])
+                ),
+                .headerSearchPath(
+                    "../../../Framework/openssl.xcframework/ios-arm64_x86_64-simulator/openssl.framework/Headers",
+                    .when(platforms: [.iOS])
+                ),
             ]
         ),
 
@@ -41,7 +51,6 @@ let package = Package(
             resources: [
                 .process("Wallet/CoreData/WalletModel.xcdatamodeld")
             ]
-            
         ),
 
         .testTarget(
